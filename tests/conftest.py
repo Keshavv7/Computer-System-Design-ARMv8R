58af26704b46cc63cdd219ae98241c3ad1e4a198
EndlessKn0t <shreevatsabhagwat@gmail.com>
2024-10-09 00:22:38 +0530

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+# conftest.py
+import pytest
+from amaranth.sim import Simulator
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '../src/')))
+from func_units.alu1 import ALU
+
+# Fixture to provide 'ctx' as a simulation context
+@pytest.fixture
+def ctx():
+    dut = ALU()
+
+    # Setup the simulator with the DUT
+    sim = Simulator(dut)
+    sim.add_clock(1e-6)  # Add a 1 MHz clock
+
+    # Return the simulator as the context
+    return sim
