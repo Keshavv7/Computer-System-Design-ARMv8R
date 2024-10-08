630031de9fc49100dd32471673500d50b9f4b211
Shreevatsa <111947350+EndlessKn0t@users.noreply.github.com>
2024-10-08 18:41:29 +0530

    Update reg.py

--- src/mem/reg.py
+++ src/mem/reg.py
@@ -5,31 +5,34 @@ class RegisterFile(Elaboratable):
         self.num_regs = num_regs
         self.width = width
 
-        # Signals for register addresses, data, and control
-        self.read_addr1 = Signal(range(num_regs))  # Address for the first read port
-        self.read_addr2 = Signal(range(num_regs))  # Address for the second read port
-        self.write_addr = Signal(range(num_regs))  # Address for the write port
-        self.write_data = Signal(width)            # Data to write
-        self.write_enable = Signal()               # Write enable signal
+        self.read_addr1 = Signal(range(num_regs))
+        self.read_addr2 = Signal(range(num_regs))
+        self.write_addr = Signal(range(num_regs))
+        self.write_data = Signal(width)
+        self.write_enable = Signal()
 
-        # Outputs for reading data
-        self.read_data1 = Signal(width)            # Data read from the first port
-        self.read_data2 = Signal(width)            # Data read from the second port
+        self.read_data1 = Signal(width)
+        self.read_data2 = Signal(width)
 
-        # Internal register storage
         self.regs = [Signal(width, reset=0) for _ in range(num_regs)]
 
     def elaborate(self, platform):
         m = Module()
 
-        # Read logic using Mux for selecting based on read address
-        m.d.comb += [
-            self.read_data1.eq(Mux(self.read_addr1, *self.regs)),
-            self.read_data2.eq(Mux(self.read_addr2, *self.regs))
-        ]
+        # Combinational read logic using Case statements
+        with m.Switch(self.read_addr1):
+            for i in range(self.num_regs):
+                with m.Case(i):
+                    m.d.comb += self.read_data1.eq(self.regs[i])
+
+        with m.Switch(self.read_addr2):
+            for i in range(self.num_regs):
+                with m.Case(i):
+                    m.d.comb += self.read_data2.eq(self.regs[i])
 
         # Synchronous write logic
         with m.If(self.write_enable):
             m.d.sync += self.regs[self.write_addr].eq(self.write_data)
 
         return m
+
