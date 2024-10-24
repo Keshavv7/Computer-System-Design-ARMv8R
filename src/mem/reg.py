2586b9e8e887256e88e6654268602db36c61a92c
Kirke <kirke@not.here>
2024-10-24 11:37:45 +0530

    Qkfix for RF

--- src/mem/reg.py
+++ src/mem/reg.py
@@ -3,8 +3,8 @@ from amaranth.lib.wiring import Component, In, Out
 from amaranth.lib.data import ArrayLayout
 
 class RegisterFile(Component):
-    read_addr1:   In(16)  # Address for the first read port
-    read_addr2:   In(16)  # Address for the second read port
+    read_addr1:   In(4)  # Address for the first read port
+    read_addr2:   In(4)  # Address for the second read port
     write_addr:   In(16)  # Address for the write port
     write_data:   In(32)  # Data to write
     write_enable: In(1)   # Write enable signal
@@ -13,27 +13,17 @@ class RegisterFile(Component):
     read_data2:   Out(32)            # Data read from the second port
 
     def elaborate(self, platform):
-        regs = [Signal(32) for _ in range(16)]
+        regs = Array([Signal(32) for _ in range(16)])
         m = Module()
 
-        # Combinational read logic using Switch and Case
-        with m.Switch(self.read_addr1):
-            for i in range(16):
-                with m.Case(i):
-                    m.d.sync += self.read_data1.eq(regs[i])
+        m.d.sync += self.read_data1.eq(regs[self.read_addr1])
 
-        with m.Switch(self.read_addr2):
-            for i in range(16):
-                with m.Case(i):
-                    m.d.sync += self.read_data2.eq(regs[i])
+        m.d.sync += self.read_data2.eq(regs[self.read_addr2])
 
         # Synchronous write logic
         with m.If(self.write_enable):
             # Using a multiplexer to determine which register to write to
-            with m.Switch(self.write_addr):
-                for i in range(16):
-                    with m.Case(i):
-                        m.d.sync += regs[i].eq(self.write_data)
+            m.d.sync += regs[self.write_addr].eq(self.write_data)
 
         return m
 
